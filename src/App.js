3b70f4bbd634ebe46fb541a9fd7cbc8075402008
Yadnyesh Borse <yadvborse27@gmail.com>
2025-12-28 01:47:24 +0530

    are

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-
+import "./App.css";
+import Header from "./MyCompoents/Header";
+import ToDoS from "./MyCompoents/ToDoS";
+import Footer from "./MyCompoents/Footer";
 
 function App() {
-  let variable = 25;
   return (
     <div className="App">
-      <nav className="navbar" style={{ display: 'flex', justifyContent: 'space-between', padding: '10px', backgroundColor: '#f0f0f0' }}>
-        <h2>Todo List</h2>
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <input type="text" placeholder="Search todos..." style={{ padding: '5px' }} />
-      </nav>
-      <header className="App-header">
-        <h1>Todo List</h1>
-        <h2>Home</h2>
-        <h2>About</h2>
-      </header>
+      <Header />
+      <ToDoS />
+      <Footer />
     </div>
   );
 }
---
+++ src/MyCompoents/ToDoS.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ToDoS() {
+  return (
+    <div>ToDo Works</div>
+  )
+}
